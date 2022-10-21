92ed244e89f3303dd1a370d291885ec3bfd6d9a9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-21 14:42:31 +0100

    chore:added btns dynamically to the project

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ function App() {
       </div>
       <div className="jobs-center">
         {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button key={item.id} onClick={() => setValue(index)}>
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
         {/* jobs info */}
         <article className="job-info">
           <h3>{title}</h3>
@@ -43,7 +52,7 @@ function App() {
           <p className="job-date">{dates}</p>
           {duties.map((duty, index) => {
             return (
-              <div className="job-desc">
+              <div key={index} className="job-desc">
                 <FaAngleDoubleRight className="job-icon" />
                 <p>{duty}</p>
               </div>
