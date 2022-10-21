69eaa4e5607b585ad2e33b3153dc595de6b584db
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-21 13:44:43 +0100

    chore:fetching the data using api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
+// import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tabs-project";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    // console.log(response);
+    setJobs(newJobs);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <h2>tabs project setup</h2>;
 }
 
